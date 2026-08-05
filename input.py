05c9c2a54fc807184c98f06ff229919183c39599
subigya-js <developer.subigya@gmail.com>
2026-08-05 08:49:15 +0545

    fix: update type checking comments for clarity in input.py

--- input.py
+++ input.py
@@ -4,9 +4,9 @@ print(type(my_name))
 
 my_age = input("Enter your age: ")
 print(my_age)
-print(type(my_age))
+print(type(my_age)) # Output: str
 
 my_height = int(input("Enter your height : "))
 # my_height = int(my_height)
 print(my_height)
-print(type(my_height))
+print(type(my_height)) # Output: int

310e4817723af5d8d1670855810563315b1a7fb8
subigya-js <developer.subigya@gmail.com>
2026-08-05 08:31:45 +0545

    feat: add python scripts demonstrating basic data types and variable usage

---
+++ data_types.py
@@ -0,0 +1,10 @@
+name = "Gaurav"
+age = 24
+cgpa = 8.91
+isStudent = True
+
+# Check type of variables
+print(type(name)) # Output: <class 'str'> --> String
+print(type(age)) # Output: <class 'int'> --> Integer
+print(type(cgpa)) # Output: <class 'float'> --> Float
+print(type(isStudent)) #Output: <class 'bool'> --> Boolean
---
+++ variables.py
@@ -0,0 +1,5 @@
+name = "Gaurav"
+age = 24
+
+print(name)
+print("My name is", name, "and I am", age, "years old.")

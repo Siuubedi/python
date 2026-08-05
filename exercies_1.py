63258d5a83453c5ac1f805ae9b5877f3423fee4f
subigya-js <developer.subigya@gmail.com>
2026-08-05 09:16:49 +0545

    feat: add exercies_1.py script demonstrating user input and string formatting

---
+++ exercies_1.py
@@ -0,0 +1,8 @@
+first_name = "Tony"
+last_name = "Stark"
+age = 53
+height = "1.85m"
+
+superhero_name = input("Enter Tony's superhero name: ")
+print(first_name, "is", age, "years old and", height,
+      "tall. His superhero name is", superhero_name)

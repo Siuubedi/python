aecdecde6757d667d1f33c31edbd01bb7fc284a2
subigya-js <developer.subigya@gmail.com>
2026-08-05 08:47:16 +0545

    feat: add input.py script demonstrating user input and type checking

---
+++ input.py
@@ -0,0 +1,12 @@
+my_name = input("Enter your name: ")
+print(my_name)
+print(type(my_name))
+
+my_age = input("Enter your age: ")
+print(my_age)
+print(type(my_age))
+
+my_height = int(input("Enter your height : "))
+# my_height = int(my_height)
+print(my_height)
+print(type(my_height))

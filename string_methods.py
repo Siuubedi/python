3f1ed902a8c1839a967cd3db6638e4a9133706ed
subigya-js <developer.subigya@gmail.com>
2026-08-05 10:47:39 +0545

    feat: add string_methods.py script demonstrating string manipulation methods

---
+++ string_methods.py
@@ -0,0 +1,11 @@
+name = "Tony Stark"
+print(name.upper())  # Output: TONY STARK
+print(name.lower())  # Output: tony stark
+
+print(name.find("ark"))  # Output: 7
+print(name.find("Ton"))  # Output: 0
+print(name.find("Hello"))  # Output: -1
+
+print(name.replace("ark", "art"))  # Output: Tony Start
+print(name.replace("Tony Stark", "Gaurav Subedi"))  # Output: Gaurav Subedi
+print(name)  # Output: Tony Stark

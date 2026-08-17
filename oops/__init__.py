d73d858275c26ea77dfac35c17166f236420878e
subigya-js <developer.subigya@gmail.com>
2026-08-17 11:50:20 +0545

    feat: add Student class with initialization and name calling methods

---
+++ oops/__init__.py
@@ -0,0 +1,20 @@
+class Student:
+    college = "Jain University"
+
+    def __init__(self, name):
+        self.name = name
+        print(self.name, "fron init function studies at", self.college)
+        print("Adding new student in DB.")
+
+    def call_name(self):
+        print(self.name, "from call_name function studies at", self.college)
+
+
+s1 = Student("Gaurav")
+s1.call_name()
+# print(s1.name) # Output: Gaurav
+
+print("---------------------------------------------------")
+
+s2 = Student("Subedi")
+s2.call_name()
---
+++ oops/basic.py
@@ -0,0 +1,10 @@
+# Creating class
+class Student:
+    name = "Gaurav Subedi"
+
+
+s1 = Student()
+print(s1.name)  # Output: Gaurav Subedi
+
+s1.name = "Subigya"
+print(s1.name) # Output: Subigya

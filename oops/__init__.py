d48a216c22d01e0db4bf4427ad6989111e7e1ab3
subigya-js <developer.subigya@gmail.com>
2026-08-17 14:57:50 +0545

    feat: add comments to clarify Student class methods

--- oops/__init__.py
+++ oops/__init__.py
@@ -6,11 +6,13 @@ class Student:
         print(self.name, "fron init function studies at", self.college)
         print("Adding new student in DB.")
 
+    # The call_name() function is the method of the Student class.
     def call_name(self):
         print(self.name, "from call_name function studies at", self.college)
 
 
 s1 = Student("Gaurav")
+# Calling method.
 s1.call_name()
 # print(s1.name) # Output: Gaurav
 
